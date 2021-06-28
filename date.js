29dd47be004a3bf8c7b135c8a6de0fffe3ac9733
David <davidtucu@hotmail.com>
2021-06-28 20:14:47 -0300

    First commit

---
+++ date.js
@@ -0,0 +1,22 @@
+
+module.exports.getDate = function(){
+    const today = new Date();
+    const options = {
+        weekday : 'long', 
+        day: 'numeric',
+        month: 'long'
+    };
+    const day = today.toLocaleDateString('en-US', options);
+    return day;
+}
+
+
+function getDay(){
+    const today = new Date();
+    const options = {
+        weekday : 'long'
+    };
+    const day = today.toLocaleDateString('en-US', options);
+    return day;
+}
+module.exports.getDay = getDay;
